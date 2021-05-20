db228f1c3f5f1e2b79cd0ea7d74c3602ff104942
CodyKlaxon <>
2021-05-20 10:55:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,161 @@
+##
+##
+##
+import time
+from datetime import date
+date = date.today()
+
+
+print("Welcome to Copington Adventure Theme Park")
+print()
+print()
+
+time.sleep(1)
+
+print("Ticket prices")
+print("_______________")
+print("Adult......................£20")
+print("Child......................£12")
+print("Senior.....................£11")
+time.sleep(1)
+print()
+time.sleep(1)
+print("Wristbands")
+print("_______________")
+print("All Access Wristband.......£20")
+time.sleep(1)
+print()
+print("Parking")
+print("_______________")
+print("Parking Pass...............Free")
+print()
+print("______________________________________________________________________________")
+print()
+time.sleep(0.5)
+print("Please enter the amount of adult tickets you would like.")
+adult_amount = int(input(" >> "))
+time.sleep(0.2)
+print("Please enter the amount of child tickets you would like.")
+child_amount = int(input(" >> "))
+time.sleep(0.2)
+print("Please enter the amount of senior tickets you would like.")
+senior_amount = int(input(" >> "))
+time.sleep(0.2)
+print("Please enter the amount of wristbands you would like.")
+wristband_amount = int(input(" >> "))
+time.sleep(0.2)
+
+def entrance(ca, aa, sa, wa):
+    aa = 20 * aa
+    ca = 12 * ca
+    sa = 11 * sa
+    wa = 20 * wa
+    total_cost = aa + ca + sa + wa
+    return total_cost
+    
+total_cost = entrance(child_amount, adult_amount, senior_amount, wristband_amount)
+print(total_cost)#debugprint
+#working
+
+time.sleep(1)
+print("______________________________________________________________________________")
+print()
+print("Please Input the lead bookers surname")
+surname = input(" >> ").upper()
+time.sleep(0.2)
+print()
+print("Do you require a parking pass")
+parking = input(" Y/N >> ").upper()
+time.sleep(0.2)
+print()
+time.sleep(0.2)
+print("______________________________________________________________________________")
+print("Your Total Cost")
+print("_______________")
+print("...")
+time.sleep(0.2)
+print("...")
+time.sleep(0.2)
+print(f"Total Cost: £ {total_cost}")
+print()
+time.sleep(1)
+
+def collect_money(total_cost, notes_entered):
+    loop = True
+    print("This machine accepts £10, and £20 notes.")
+    time.sleep(0.5)
+    print("Please enter each note.")
+    while loop:
+      print(f"Current amount due: {total_cost}")
+      notes_entered = int(input("Please Input a 10, or a 20. >> "))
+      print()
+      if notes_entered == 10:
+         total_cost -= 10
+      if notes_entered == 20:
+         total_cost -= 20
+      if total_cost <= 0:
+         print()
+         time.sleep(1)
+         print("Thank You, You have paid the full amount")
+         change = total_cost
+         time.sleep(1)
+         print()
+         print(f"Your change is: £{abs(change)}")
+         loop = False
+
+notes_entered = 0
+collect_money(total_cost, notes_entered)
+#total_cost = collect_money(total_cost, notes_entered)
+
+#Print a ticket (display lead booker surname, tickets purchased, wristbands purchased, today’s date*)
+
+print()
+print("Please wait while we print your ticket.")
+print()
+time.sleep(3)
+print("_________________COPINGTON ADVENTURE THEME PARK_________________________")
+time.sleep(0.2)
+print("________________________TICKET FOR ENTRY _______________________________")
+time.sleep(0.2)
+print(f"________BOOKER:{surname}___________________DATE:{date.today()}_______________")
+time.sleep(0.2)
+print(f"______ADULT TICKETS PURCHASED:{adult_amount}_________CHILD TICKETS PURCHASED:{child_amount}______")
+time.sleep(0.2)
+print(f"_____SENIOR TICKETS PURCHASED:{senior_amount}________WRISTBANDS PURCHASED:{wristband_amount}__________")
+time.sleep(0.2)
+print("________________/______-________________________/_\_____________________")
+time.sleep(0.2)
+print("______________/___\__/___\_______/_-_\________/_____\___________________")
+time.sleep(0.2)
+print("____________/_______\______-_-_/_______\/_-_-_________\_________________")
+time.sleep(0.2)
+print()
+time.sleep(0.2)
+print()
+time.sleep(0.2)
+print()
+time.sleep(0.2)
+print()
+time.sleep(0.2)
+
+if parking == "Y":
+    time.sleep(2)
+    print("Please wait while we print your parking pass")
+    print()
+    time.sleep(3)
+
+    print("_________________COPINGTON ADVENTURE THEME PARK_________________")
+    time.sleep(0.2)
+    print("________________________PARKING PASS____________________________")
+    time.sleep(0.2)
+    print("________________________________________________________________")
+    time.sleep(0.2)
+    print(f"___________________VALID ONLY ON:{date.today()}_____________________")
+    time.sleep(0.2)
+    print("________________________________________________________________")
+    time.sleep(0.2)
+    print("________________________________________________________________")
+    time.sleep(0.2)
+
+print()
+print("Thank You for your Purchase!")
