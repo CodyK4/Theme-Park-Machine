36a06a5b7692340dd38daa6d59dc5b9a72627137
Cody K <>
2021-05-21 11:42:21 +0000

    Added Adminpanel, Cleaned up code, Fixed problems.

--- main.py
+++ main.py
@@ -1,9 +1,122 @@
-##
-##
-##
 import time
+import replit 
 from datetime import date
 date = date.today()
+
+##
+##
+##
+
+##FUNCTIONS
+
+def adminpanel():#childcost, adultcost, seniorcost
+          cc = True
+          ac = True
+          sc = True
+          print("Welcome Administrator")
+          print()
+          print("Would you like to edit ticket prices?")
+          print()
+          q1 = input(" Y/N >> ").upper()
+          time.sleep(0.5)
+          if q1 == "Y":
+              while cc:
+                try:
+                  #childcost = 18
+                  childcost = int(input("Input Child Entry Price >>  £"))
+                  print()
+                  cc = False
+                except ValueError: 
+                    print("You must enter a number.")
+              while ac:
+                try:
+                  #adultcost = 19
+                  adultcost = int(input("Input Adult Entry Price >>  £"))
+                  print()
+                  ac = False
+                except ValueError:
+                  print("You must enter a number.")
+              while sc:
+                try:
+                  #seniorcost = 20
+                  seniorcost = int(input("Input Senior Entry Price >>  £"))
+                  print()
+                  sc = False
+                except ValueError:
+                    print("You must enter a number")
+          else:
+              time.sleep(0.2)
+              print()
+              print("Thank you for using AdminPanel")
+
+          return childcost, adultcost, seniorcost
+
+def fticketsleft(ticketsleft, ca, aa, sa):
+    ticketsleft -= aa
+    ticketsleft -= ca
+    ticketsleft -= sa
+    return ticketsleft
+
+def entrance(ca, aa, sa, wa, cc, ac, sc):
+    aa = ac * aa
+    ca = cc * ca
+    sa = sc * sa
+    wa = 20 * wa
+    total_cost = aa + ca + sa + wa
+    return total_cost
+
+def collect_money(total_cost, notes_entered):
+    loop = True
+    print("This machine accepts £10, and £20 notes.")
+    time.sleep(0.5)
+    print("Please enter each note.")
+    while loop:
+      print(f"Current amount due: £{total_cost}")
+      notes_entered = int(input("Please Input a 10, or a 20. >> "))
+      print()
+      if notes_entered == 2525: #DEBUG
+         total_cost -= 5000
+      if notes_entered == 10:
+         total_cost -= 10
+      if notes_entered == 20:
+         total_cost -= 20
+      if total_cost <= 0:
+          print()
+          time.sleep(1)
+          print("Thank You, You have paid the full amount")
+          change = total_cost
+          time.sleep(1)
+          print()
+          print(f"Your change is: £{abs(change)}")
+          loop = False
+      else:
+        time.sleep(0.5)
+        print("Please only use £10 or £20 notes. ")
+        print()
+
+#FUNCTIONS
+
+##
+##
+##
+
+##
+##
+##
+
+#ticketsleft
+ticketsleft = 500
+
+#ticketcost
+childcost = 12
+adultcost = 20
+seniorcost = 11
+
+#VARIABLES
+
+##
+##
+##
 
 godloop = True
 while godloop:
@@ -12,15 +125,25 @@ while godloop:
     print()
 
     masterloop = True
-    machineruntotal = 500
-    print(f"There are {machineruntotal} tickets left.")
+    print(f"There are {ticketsleft} tickets left.")
     print()
 
     time.sleep(1)
 
     print("Press E to wake the machine up.")
-    masterlooptrigger = input()
-
+    masterlooptrigger = input().upper()
+    
+    if masterlooptrigger == "ADMIN":
+        adminpanel_childcost, adminpanel_adultcost, adminpanel_seniorcost = adminpanel()
+        childcost = adminpanel_childcost
+        adultcost = adminpanel_adultcost
+        seniorcost = adminpanel_seniorcost 
+        time.sleep(0.4)
+        print()
+        print(f"Child Ticket cost is now set to: £{childcost}")
+        print(f"Adult Ticket cost is now set to: £{adultcost}")
+        print(f"Senior Ticket cost is now set to: £{seniorcost}")
+        print()
 
     if masterlooptrigger == "E":
       print()
@@ -31,9 +154,9 @@ while godloop:
       while masterloop:
         print("Ticket prices")
         print("_______________")
-        print("Adult......................£20")
-        print("Child......................£12")
-        print("Senior.....................£11")
+        print(f"Adult......................£{adultcost}")
+        print(f"Child......................£{childcost}")
+        print(f"Senior.....................£{seniorcost}")
         time.sleep(1)
         print()
         time.sleep(1)
@@ -93,17 +216,8 @@ while godloop:
             print("Please input a number")
         time.sleep(0.2)
 
-        def entrance(ca, aa, sa, wa):
-            aa = 20 * aa
-            ca = 12 * ca
-            sa = 11 * sa
-            wa = 20 * wa
-            total_cost = aa + ca + sa + wa
-            return total_cost
-            
-        total_cost = entrance(child_amount, adult_amount, senior_amount, wristband_amount)
-        #print(total_cost)#debugprint
-
+        total_cost = entrance(child_amount, adult_amount, senior_amount, wristband_amount, childcost, adultcost, seniorcost)
+        ticketsleft = fticketsleft(ticketsleft, child_amount, adult_amount, senior_amount)
 
         time.sleep(1)
         print("______________________________________________________________________________")
@@ -122,8 +236,8 @@ while godloop:
         time.sleep(0.2)
         print()
 
-        print("Do you require a parking pass")
-        parking = input(" Y/N >> ").upper()
+        print("Do you require a parking pass?")
+        parking = str(input(" Y/N >> ").upper())
 
         time.sleep(0.2)
         print()
@@ -139,32 +253,7 @@ while godloop:
         print()
         time.sleep(1)
 
-        def collect_money(total_cost, notes_entered):
-            loop = True
-            print("This machine accepts £10, and £20 notes.")
-            time.sleep(0.5)
-            print("Please enter each note.")
-            while loop:
-              print(f"Current amount due: {total_cost}")
-              notes_entered = int(input("Please Input a 10, or a 20. >> "))
-              print()
-              if notes_entered == 10:
-                total_cost -= 10
-              if notes_entered == 20:
-                total_cost -= 20
-              if total_cost <= 0:
-                print()
-                time.sleep(1)
-                print("Thank You, You have paid the full amount")
-                change = total_cost
-                time.sleep(1)
-                print()
-                print(f"Your change is: £{abs(change)}")
-                loop = False
-              else:
-                time.sleep(0.5)
-                print("Please only use £10 or £20 notes. ")
-                print()
+        
 
         notes_entered = 0
         collect_money(total_cost, notes_entered)
@@ -225,7 +314,7 @@ while godloop:
         print()
         print("This machine will reset in 20 seconds.")
         time.sleep(10)
-        print("This machine will reset in 10 seconds")
+        print("This machine will reset in 10 seconds.")
         time.sleep(5)
         print("This machine will reset in 5 seconds.")
         time.sleep(5)
@@ -233,8 +322,16 @@ while godloop:
         print()
         print()
         print()
-        machineruntotal = machineruntotal - 1
+        replit.clear()
         masterloop = False
+      if ticketsleft <= 0:
+        print("_____________Welcome to Copington Adventure Theme Park_____________")
+        print()
+        print()
+
+        print("Unfortunately, We are out of tickets. We are sorry for Any Inconvience.")
+        godloop = False
+
 
 
 #NOTES
